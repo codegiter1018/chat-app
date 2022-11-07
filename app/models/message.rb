ed62fdb2c928db7c17f00742b8a493c7c6f8f4b1
codegiter1018 <tekku1te2tete@gmail.com>
2022-11-07 18:37:11 +0900

    画像のみでもメッセージ送信可能となるバリテーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,5 +5,9 @@ class Message < ApplicationRecord
 
 
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
+  def was_attached?
+    self.image.attached?
+  end
+
 end
